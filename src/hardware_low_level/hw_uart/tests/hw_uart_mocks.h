ff13f35bcdedef7bd8024bd46ba5a6405a146f70
CallumR2 <craf0002@student.monash.edu>
2026-04-08 20:54:43 +1000

    DEV-70 add UART RX  start path whith HAL init and DMA. Completed peek and consuume pipeline.

--- src/hardware_low_level/hw_uart/tests/hw_uart_mocks.h
+++ src/hardware_low_level/hw_uart/tests/hw_uart_mocks.h
@@ -31,14 +31,48 @@ extern "C"
  *------------------------------------------------------------------------------
  */
 
+#define UART_WORDLENGTH_8B 8U
+#define UART_WORDLENGTH_9B 9U
+
+#define UART_STOPBITS_1 1U
+#define UART_STOPBITS_2 2U
+
+#define UART_PARITY_NONE 0U
+#define UART_PARITY_EVEN 1U
+#define UART_PARITY_ODD 2U
+
+#define UART_MODE_RX 0x01U
+#define UART_MODE_TX 0x02U
+#define UART_MODE_TX_RX ( UART_MODE_RX | UART_MODE_TX )
+
 /**-----------------------------------------------------------------------------
  *  Public Typedefs / Enums / Structures
  *------------------------------------------------------------------------------
  */
+typedef struct
+{
+    volatile uint32_t SR;   /*!< USART Status register,                Address offset: 0x00 */
+    volatile uint32_t DR;   /*!< USART Data register,                  Address offset: 0x04 */
+    volatile uint32_t BRR;  /*!< USART Baud rate register,             Address offset: 0x08 */
+    volatile uint32_t CR1;  /*!< USART Control register 1,             Address offset: 0x0C */
+    volatile uint32_t CR2;  /*!< USART Control register 2,             Address offset: 0x10 */
+    volatile uint32_t CR3;  /*!< USART Control register 3,             Address offset: 0x14 */
+    volatile uint32_t GTPR; /*!< USART Guard time and prescaler reg,   Address offset: 0x18 */
+} USART_TypeDef;
 
 typedef struct
 {
-    void* Instance;
+    uint32_t BaudRate;
+    uint32_t WordLength;
+    uint32_t StopBits;
+    uint32_t Parity;
+    uint32_t Mode;
+} UART_InitTypeDef;
+
+typedef struct
+{
+    USART_TypeDef*   Instance;
+    UART_InitTypeDef Init;
 } UART_HandleTypeDef;
 
 typedef enum
@@ -53,17 +87,6 @@ typedef struct
 {
     uint32_t NDTR;
 } DMA_Stream_TypeDef;
-
-typedef struct
-{
-    volatile uint32_t SR;   /*!< USART Status register,                Address offset: 0x00 */
-    volatile uint32_t DR;   /*!< USART Data register,                  Address offset: 0x04 */
-    volatile uint32_t BRR;  /*!< USART Baud rate register,             Address offset: 0x08 */
-    volatile uint32_t CR1;  /*!< USART Control register 1,             Address offset: 0x0C */
-    volatile uint32_t CR2;  /*!< USART Control register 2,             Address offset: 0x10 */
-    volatile uint32_t CR3;  /*!< USART Control register 3,             Address offset: 0x14 */
-    volatile uint32_t GTPR; /*!< USART Guard time and prescaler reg,   Address offset: 0x18 */
-} USART_TypeDef;
 
 /**-----------------------------------------------------------------------------
  *  Mock Peripheral Instances
@@ -88,11 +111,15 @@ static DMA_Stream_TypeDef DMA1_Stream6_mock = { 0U };
 #define DMA1_Stream5 ( &DMA1_Stream5_mock )
 #define DMA1_Stream6 ( &DMA1_Stream6_mock )
 
+// hUart handle mocks
+static UART_HandleTypeDef huart1 = { .Instance = USART1 };
+static UART_HandleTypeDef huart2 = { .Instance = USART2 };
 /**-----------------------------------------------------------------------------
  *  Public Function Prototypes
  *------------------------------------------------------------------------------
  */
 
+HAL_StatusTypeDef HAL_UART_Init( UART_HandleTypeDef* huart );
 HAL_StatusTypeDef HAL_UART_Transmit_DMA( UART_HandleTypeDef* huart, uint8_t* pData, uint16_t Size );
 HAL_StatusTypeDef HAL_UART_Receive_DMA( UART_HandleTypeDef* huart, uint8_t* pData, uint16_t Size );
 
