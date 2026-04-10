dfe849500fafa96872f35ed868eb415b7db5d224
Angus Corr Desktop <angus.corr@monash.edu>
2026-04-10 12:14:56 +1000

    DEV-78 SPI
    - Initial Typedefs in Low Level Hardware Driver
    - .ioc adjustments and generation of drivers

--- src/hardware_low_level/hw_spi/hw_spi.h
+++ src/hardware_low_level/hw_spi/hw_spi.h
@@ -36,6 +36,48 @@ extern "C"
  *  Public Typedefs / Enums / Structures
  *------------------------------------------------------------------------------
  */
+typedef enum SPIBaudRate_T
+{
+    SPI_BAUD_45MBIT,
+    SPI_BAUD_22M5BIT,
+    SPI_BAUD_11M25BIT,
+    SPI_BAUD_5M625BIT,
+    SPI_BAUD_2M813BIT,
+    SPI_BAUD_1M406BIT,
+    SPI_BAUD_703KBIT,
+    SPI_BAUD_352KBIT,
+} SPIBaudRate_T;
+
+typedef enum SPICPOL_T
+{
+    SPI_CPOL_LOW,
+    SPI_CPOL_HIGH,
+} SPICPOL_T;
+
+typedef enum SPICPHA_T
+{
+    SPI_CPHA_1_EDGE,
+    SPI_CPHA_2_EDGE,
+} SPICPHA_T;
+
+typedef enum SPIDataSize_T
+{
+    SPI_SIZE_8_BIT,
+    SPI_SIZE_16_BIT,
+} SPIDataSize_T;
+
+typedef enum SPIFirstBit_T
+{
+    SPI_FIRST_MSB,
+    SPI_FIRST_LSB,
+} SPIFirstBit_T;
+
+typedef enum SPIPeripheral_T
+{
+    SPI_CHANNEL_0,
+    SPI_CHANNEL_1,
+    SPI_DAC,
+} SPIPeripheral_T;
 
 /**-----------------------------------------------------------------------------
  *  Public Function Prototypes
