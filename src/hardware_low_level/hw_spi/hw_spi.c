ac1d70df21c674d1f42444bb7279fb22e06aab02
Angus Corr Desktop <angus.corr@monash.edu>
2026-04-11 18:13:34 +1000

    DEV-78 SPI
    - Regenerated .ioc with SPI examples
    - completed configuration function for SPI

--- src/hardware_low_level/hw_spi/hw_spi.c
+++ src/hardware_low_level/hw_spi/hw_spi.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *  File:       hw_spi.c
- *  Author:     Callum Rafferty
- *  Created:    25-Mar-2026
+ *  Author:     Angus Corr
+ *  Created:    10-Apr-2026
  *
  *  Description:
  *      <Short description of the module's purpose and responsibilities>
@@ -15,7 +15,14 @@
  *------------------------------------------------------------------------------
  */
 
+#ifdef TEST_BUILD
+#include "tests/hw_spi_mocks.h"
+#else
+#include "spi.h"
+#include "stm32f446xx.h"
+#endif
 #include "hw_spi.h"
+#include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 // Add other required includes here
@@ -24,6 +31,14 @@
  *  Defines / Macros
  *------------------------------------------------------------------------------
  */
+// TODO: Change this based on true hardware
+#define SPI_CHANNEL_0_HANDLE hspi1
+#define SPI_CHANNEL_1_HANDLE hspi4
+#define SPI_DAC_HANDLE hspi4
+
+#define SPI_CHANNEL_0_INSTANCE SPI1
+#define SPI_CHANNEL_1_INSTANCE SPI4
+#define SPI_DAC_INSTANCE SPI4
 
 /**-----------------------------------------------------------------------------
  *  Typedefs / Enums / Structures
@@ -54,3 +69,139 @@
  *  Public Function Definitions
  *------------------------------------------------------------------------------
  */
+
+bool HW_SPI_Configure_Channel( SPIPeripheral_T peripheral, HWSPIConfig_T configuration )
+{
+    SPI_HandleTypeDef* hspi = NULL;
+    // SPI Channel
+    switch ( peripheral )
+    {
+        case SPI_CHANNEL_0:
+            hspi = &SPI_CHANNEL_0_HANDLE;
+            hspi->Instance = SPI_CHANNEL_0_INSTANCE;
+            break;
+        case SPI_CHANNEL_1:
+            hspi = &SPI_CHANNEL_1_HANDLE;
+            hspi->Instance = SPI_CHANNEL_1_INSTANCE;
+            break;
+        case SPI_DAC:
+            hspi = &SPI_DAC_HANDLE;
+            hspi->Instance = SPI_DAC_INSTANCE;
+            break;
+        default:
+            return false;
+    }    
+
+    // Mode + chip select logic
+    switch ( configuration.spi_mode )
+    {
+        case SPI_MASTER_MODE:
+            hspi->Init.Mode = SPI_MODE_MASTER;
+            hspi->Init.NSS = SPI_NSS_HARD_OUTPUT;
+            break;
+        case SPI_SLAVE_MODE:
+            hspi->Init.Mode = SPI_MODE_SLAVE;
+            hspi->Init.NSS = SPI_NSS_HARD_INPUT;
+            break;
+        default:
+            return false;
+    }
+
+    // Direction (always 2 lines (MISO and MOSI))
+    hspi->Init.Direction = SPI_DIRECTION_2LINES;
+    // Datasize (8 bit or 16 bit)
+    switch ( configuration.data_size )
+    {
+        case SPI_SIZE_8_BIT:
+            hspi->Init.DataSize = SPI_DATASIZE_8BIT;
+            break;
+        case SPI_SIZE_16_BIT:
+            hspi->Init.DataSize = SPI_DATASIZE_16BIT;
+            break;
+        default:
+            return false;
+    }
+
+    // Clock Polarity (CPOL)
+    switch ( configuration.cpol )
+    {
+        case SPI_CPOL_HIGH:
+            hspi->Init.CLKPolarity = SPI_POLARITY_HIGH;
+            break;
+        case SPI_CPOL_LOW:
+            hspi->Init.CLKPolarity = SPI_POLARITY_LOW;
+            break;
+        default:
+            return false;
+    }
+
+    // Clock Phase (CPHA)
+    switch ( configuration.cpha )
+    {
+        case SPI_CPHA_1_EDGE:
+            hspi->Init.CLKPhase = SPI_PHASE_1EDGE;
+            break;
+        case SPI_CPHA_2_EDGE:
+            hspi->Init.CLKPhase = SPI_PHASE_2EDGE;
+            break;
+        default:
+            return false;
+    }
+
+    // Baud rate
+    switch ( configuration.baud_rate )
+    {
+        case SPI_BAUD_45MBIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+            break;
+        case SPI_BAUD_22M5BIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+            break;
+        case SPI_BAUD_11M25BIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+            break;
+        case SPI_BAUD_5M625BIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+            break;
+        case SPI_BAUD_2M813BIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+            break;
+        case SPI_BAUD_1M406BIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+            break;
+        case SPI_BAUD_703KBIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
+            break;
+        case SPI_BAUD_352KBIT:
+            hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+            break;
+        default:
+            return false;
+    }
+
+    // MSB vs LSB
+    switch ( configuration.first_bit )
+    {
+        case SPI_FIRST_LSB:
+            hspi->Init.FirstBit = SPI_FIRSTBIT_LSB;
+            break;
+        case SPI_FIRST_MSB:
+            hspi->Init.FirstBit = SPI_FIRSTBIT_MSB;
+            break;
+        default:
+            return false;
+    }
+
+    // Default values sourced from HAL
+    hspi->Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi->Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi->Init.CRCPolynomial = 10;
+    if (HAL_SPI_Init(&hspi1) != HAL_OK)
+    {
+        return false;
+    }
+
+    return true;
+    
+}
+
