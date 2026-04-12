b07ea9e895c184c3b4b5cb1f04231d9fb400dd5a
CallumR2 <craf0002@student.monash.edu>
2026-04-12 14:04:33 +1000

    DEV-70 Revert to IOC of main, then added USART6 with DMA

--- f446ze_cubeide_project/Core/Inc/main.h
+++ f446ze_cubeide_project/Core/Inc/main.h
@@ -107,6 +107,16 @@ void Error_Handler(void);
 #define DUT_UART_CH1_RX_GPIO_Port GPIOC
 #define Test_GPIO_Output_Pin GPIO_PIN_8
 #define Test_GPIO_Output_GPIO_Port GPIOC
+#define USB_SOF_Pin GPIO_PIN_8
+#define USB_SOF_GPIO_Port GPIOA
+#define USB_VBUS_Pin GPIO_PIN_9
+#define USB_VBUS_GPIO_Port GPIOA
+#define USB_ID_Pin GPIO_PIN_10
+#define USB_ID_GPIO_Port GPIOA
+#define USB_DM_Pin GPIO_PIN_11
+#define USB_DM_GPIO_Port GPIOA
+#define USB_DP_Pin GPIO_PIN_12
+#define USB_DP_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
