514d82a73c1e92bef4c458e783671c455eed3a01
CallumR2 <craf0002@student.monash.edu>
2026-04-16 13:42:24 +1000

    DEV-70 - Changes to IOC, introduced loopback testing

--- f446ze_cubeide_project/Core/Inc/usart.h
+++ f446ze_cubeide_project/Core/Inc/usart.h
@@ -32,10 +32,6 @@ extern "C" {
 
 /* USER CODE END Includes */
 
-extern UART_HandleTypeDef huart4;
-
-extern UART_HandleTypeDef huart5;
-
 extern UART_HandleTypeDef huart2;
 
 extern UART_HandleTypeDef huart3;
@@ -46,8 +42,6 @@ extern UART_HandleTypeDef huart6;
 
 /* USER CODE END Private defines */
 
-void MX_UART4_Init(void);
-void MX_UART5_Init(void);
 void MX_USART2_UART_Init(void);
 void MX_USART3_UART_Init(void);
 void MX_USART6_UART_Init(void);
