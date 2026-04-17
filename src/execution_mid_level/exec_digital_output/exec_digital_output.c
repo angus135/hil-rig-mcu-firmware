738eafd67f6347f86de4b1559b4b5e8ea494af7e
tvog0001 <166362198+tvog0001@users.noreply.github.com>
2026-04-17 19:03:04 +1000

    DEV-68 Low level digital output driver
    Update outwardly facing functions

--- src/execution_mid_level/exec_digital_output/exec_digital_output.c
+++ src/execution_mid_level/exec_digital_output/exec_digital_output.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *  File:       exec_digital_output.c
- *  Author:     Angus Corr
+ *  Author:     Angus Corr, Tim V
  *  Created:    25-Mar-2026
  *
  *  Description:
@@ -17,7 +17,6 @@
 
 #include "exec_digital_output.h"
 #include "hardware_low_level/hw_gpio/hw_gpio.h"
-#include "hw_gpio.h"
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -60,10 +59,10 @@
  * @brief Sets the state of all digital outputs in a GPIO Port using the underlying GPIO HW
 functions.
  *
- * @param gpio_pack   Carrys the information about which pins to set and which port to set them on
+ * @param pin   the name of the pin that will be set
  *
  *
- * This function wraps the HW_GPIO_SetToPort( ... ) function.
+ * This function wraps the HW_GPIO_set_pin( ... ) function.
  * It can be used to set a single output pin or many output pins (on the same port).
  * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) sets LL_GPIO_PIN_5 of port A
 high
@@ -77,9 +76,25 @@ LL_GPIO_PIN_5 and LL_GPIO_PIN_4 of port A high
  * By doing so, we can set all the outputs in a single hardware access.
  * functions exist in hardware_low_level/hw_gpio.c to help manage ports
  */
-inline void GPIO_SetToPort( GPIO_PORT_PACKET gpio_pack )
+inline void EXEC_set_pin( GPIO_OUTPUT_NAMES pin)
 {
-    HW_GPIO_SetToPort( gpio_pack.gpiox, gpio_pack.pin_mask );
+    HW_GPIO_set_pin( pin );
+}
+
+/**
+ * @brief Sets the state of all digital outputs pins given to it, regardless of their port
+functions.
+ *
+ * @param pins   list of pin names
+ * @param length   the number of pin names in pins
+ *
+ * This function wraps the HW_GPIO_set_many_pins( ... ) function.
+ * similar examples to EXEC_set_pin()
+ * functions exist in hardware_low_level/hw_gpio.c to help manage ports
+ */
+inline void EXEC_set_many_pins( GPIO_OUTPUT_NAMES* pins, uint16_t length)
+{
+    HW_GPIO_set_many_pins( pins, length );
 }
 
 /**
@@ -91,19 +106,35 @@ functions.
  *
  * This function wraps the HW_GPIO_SetToPort( ... ) function.
  * It can be used to set a single output pin or many output pins (on the same port).
- * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) sets LL_GPIO_PIN_5 of port A
+ * EXAMPLE: EXEC_reset_pin( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) resets LL_GPIO_PIN_5 of port A
 high
- * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = (LL_GPIO_PIN_5 | LL_GPIO_PIN_4) } ) sets
+ * EXAMPLE: EXEC_reset_pin( {gpiox = GPIOA, pin_mask = (LL_GPIO_PIN_5 | LL_GPIO_PIN_4) } ) resets
 LL_GPIO_PIN_5 and LL_GPIO_PIN_4 of port A high
  * Setting multiple pins works because LL_GPIO_PIN_5 and LL_GPIO_PIN_4 are just uint32_t
  * in this case likely 0x0000_0020 0x0000_0010, so 0x0000_0030 is written to the BSR register
- * 0x0000_0030 = 0000_0000_0000_0000_0000_0000_0011_0000 setting pins 4 and 5 high
+ * 0x0000_0030 = 0000_0000_0000_0000_0000_0000_0011_0000 setting pins 4 and 5 low
  * mocked using GoogleMock.
  * Note: This implementation assumes all digital outputs are on the same GPIO port.
  * By doing so, we can set all the outputs in a single hardware access.
  * functions exist in hardware_low_level/hw_gpio.c to help manage ports
  */
-inline void GPIO_ResetToPort( GPIO_PORT_PACKET gpio_pack )
+inline void EXEC_reset_pin( GPIO_OUTPUT_NAMES pin )
 {
-    HW_GPIO_ResetToPort( gpio_pack.gpiox, gpio_pack.pin_mask );
+    HW_GPIO_reset_pin( pin );
+}
+
+/**
+ * @brief Resets the state of all digital outputs pins given to it, regardless of their port
+functions.
+ *
+ * @param pins   list of pin names
+ * @param length   the number of pin names in pins
+ *
+ * This function wraps the HW_GPIO_reset_many_pins( ... ) function.
+ * similar examples to EXEC_reset_pin()
+ * functions exist in hardware_low_level/hw_gpio.c to help manage ports
+ */
+inline void EXEC_reset_many_pins( GPIO_OUTPUT_NAMES* pins, uint16_t length)
+{
+    HW_GPIO_reset_many_pins( pins, length );
 }
--- src/execution_mid_level/exec_digital_output/exec_digital_output.h
+++ src/execution_mid_level/exec_digital_output/exec_digital_output.h
@@ -43,51 +43,76 @@ extern "C"
  */
 
 /**
-* @brief Sets the state of all digital outputs in a GPIO Port using the underlying GPIO HW
+ * @brief Sets the state of all digital outputs in a GPIO Port using the underlying GPIO HW
 functions.
-*
-* @param gpio_pack   Carrys the information about which pins to set and which port to set them on
-*
-*
-* This function wraps the HW_GPIO_SetToPort( ... ) function.
-* It can be used to set a single output pin or many output pins (on the same port).
-* EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) sets LL_GPIO_PIN_5 of port A
+ *
+ * @param pin   the name of the pin that will be set
+ *
+ *
+ * This function wraps the HW_GPIO_set_pin( ... ) function.
+ * It can be used to set a single output pin or many output pins (on the same port).
+ * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) sets LL_GPIO_PIN_5 of port A
 high
-* EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = (LL_GPIO_PIN_5 | LL_GPIO_PIN_4) } ) sets
+ * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = (LL_GPIO_PIN_5 | LL_GPIO_PIN_4) } ) sets
 LL_GPIO_PIN_5 and LL_GPIO_PIN_4 of port A high
-* Setting multiple pins works because LL_GPIO_PIN_5 and LL_GPIO_PIN_4 are just uint32_t
-* in this case likely 0x0000_0020 0x0000_0010, so 0x0000_0030 is written to the BSR register
-* 0x0000_0030 = 0000_0000_0000_0000_0000_0000_0011_0000 setting pins 4 and 5 high
-* mocked using GoogleMock.
-* Note: This implementation assumes all digital outputs are on the same GPIO port.
-* By doing so, we can set all the outputs in a single hardware access.
-* functions exist in hardware_low_level/hw_gpio.c to help manage ports
-*/
-inline void GPIO_SetToPort( GPIO_PORT_PACKET gpio_pack )
+ * Setting multiple pins works because LL_GPIO_PIN_5 and LL_GPIO_PIN_4 are just uint32_t
+ * in this case likely 0x0000_0020 0x0000_0010, so 0x0000_0030 is written to the BSR register
+ * 0x0000_0030 = 0000_0000_0000_0000_0000_0000_0011_0000 setting pins 4 and 5 high
+ * mocked using GoogleMock.
+ * Note: This implementation assumes all digital outputs are on the same GPIO port.
+ * By doing so, we can set all the outputs in a single hardware access.
+ * functions exist in hardware_low_level/hw_gpio.c to help manage ports
+ */
+inline void EXEC_set_pin( GPIO_OUTPUT_NAMES pin);
 
-    /**
-     * @brief Resets the state of all digital outputs in a GPIO Port using the underlying GPIO HW
-    functions.
-     *
-     * @param gpio_pack   Carrys the information about which pins to set and which port to set them
-    on
-     *
-     *
-     * This function wraps the HW_GPIO_SetToPort( ... ) function.
-     * It can be used to set a single output pin or many output pins (on the same port).
-     * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) sets LL_GPIO_PIN_5 of
-    port A high
-     * EXAMPLE: GPIO_SetToPort( {gpiox = GPIOA, pin_mask = (LL_GPIO_PIN_5 | LL_GPIO_PIN_4) } ) sets
-    LL_GPIO_PIN_5 and LL_GPIO_PIN_4 of port A high
-     * Setting multiple pins works because LL_GPIO_PIN_5 and LL_GPIO_PIN_4 are just uint32_t
-     * in this case likely 0x0000_0020 0x0000_0010, so 0x0000_0030 is written to the BSR register
-     * 0x0000_0030 = 0000_0000_0000_0000_0000_0000_0011_0000 setting pins 4 and 5 high
-     * mocked using GoogleMock.
-     * Note: This implementation assumes all digital outputs are on the same GPIO port.
-     * By doing so, we can set all the outputs in a single hardware access.
-     * functions exist in hardware_low_level/hw_gpio.c to help manage ports
-     */
-    inline void GPIO_ResetToPort( GPIO_PORT_PACKET gpio_pack )
+/**
+ * @brief Sets the state of all digital outputs pins given to it, regardless of their port
+functions.
+ *
+ * @param pins   list of pin names
+ * @param length   the number of pin names in pins
+ *
+ * This function wraps the HW_GPIO_set_many_pins( ... ) function.
+ * similar examples to EXEC_set_pin()
+ * functions exist in hardware_low_level/hw_gpio.c to help manage ports
+ */
+inline void EXEC_set_many_pins( GPIO_OUTPUT_NAMES* pins, uint16_t length);
+
+/**
+ * @brief Resets the state of all digital outputs in a GPIO Port using the underlying GPIO HW
+functions.
+ *
+ * @param gpio_pack   Carrys the information about which pins to set and which port to set them on
+ *
+ *
+ * This function wraps the HW_GPIO_SetToPort( ... ) function.
+ * It can be used to set a single output pin or many output pins (on the same port).
+ * EXAMPLE: EXEC_reset_pin( {gpiox = GPIOA, pin_mask = LL_GPIO_PIN_5} ) resets LL_GPIO_PIN_5 of port A
+high
+ * EXAMPLE: EXEC_reset_pin( {gpiox = GPIOA, pin_mask = (LL_GPIO_PIN_5 | LL_GPIO_PIN_4) } ) resets
+LL_GPIO_PIN_5 and LL_GPIO_PIN_4 of port A high
+ * Setting multiple pins works because LL_GPIO_PIN_5 and LL_GPIO_PIN_4 are just uint32_t
+ * in this case likely 0x0000_0020 0x0000_0010, so 0x0000_0030 is written to the BSR register
+ * 0x0000_0030 = 0000_0000_0000_0000_0000_0000_0011_0000 setting pins 4 and 5 low
+ * mocked using GoogleMock.
+ * Note: This implementation assumes all digital outputs are on the same GPIO port.
+ * By doing so, we can set all the outputs in a single hardware access.
+ * functions exist in hardware_low_level/hw_gpio.c to help manage ports
+ */
+inline void EXEC_reset_pin( GPIO_OUTPUT_NAMES pin );
+
+/**
+ * @brief Resets the state of all digital outputs pins given to it, regardless of their port
+functions.
+ *
+ * @param pins   list of pin names
+ * @param length   the number of pin names in pins
+ *
+ * This function wraps the HW_GPIO_reset_many_pins( ... ) function.
+ * similar examples to EXEC_reset_pin()
+ * functions exist in hardware_low_level/hw_gpio.c to help manage ports
+ */
+inline void EXEC_reset_many_pins( GPIO_OUTPUT_NAMES* pins, uint16_t length);
 
 #ifdef __cplusplus
 }
