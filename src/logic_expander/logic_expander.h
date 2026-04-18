51a2fda688c5446c2f2f766b3b085e3b4f61ef6d
cpas0004 <cpas0004@student.monash.edu>
2026-04-18 14:11:36 +1000

    DEV-79 I2C and Logic Expander

--- src/logic_expander/logic_expander.h
+++ src/logic_expander/logic_expander.h
@@ -30,15 +30,50 @@ extern "C"
  *------------------------------------------------------------------------------
  */
 
+#define LOGIC_EXPANDER_COUNT ( 8U )
+#define LOGIC_EXPANDER_PORT_WIDTH_BITS ( 8U )
+
 /**-----------------------------------------------------------------------------
  *  Public Typedefs / Enums / Structures
  *------------------------------------------------------------------------------
  */
 
+typedef enum LogicExpanderPort_T
+{
+    LOGIC_EXPANDER_PORT_A,
+    LOGIC_EXPANDER_PORT_B,
+} LogicExpanderPort_T;
+
+typedef enum LogicExpanderStatus_T
+{
+    LOGIC_EXPANDER_STATUS_OK,
+    LOGIC_EXPANDER_STATUS_BUSY,
+    LOGIC_EXPANDER_STATUS_ERROR,
+    LOGIC_EXPANDER_STATUS_INVALID_PARAM,
+    LOGIC_EXPANDER_STATUS_NOT_READY,
+} LogicExpanderStatus_T;
+
+typedef struct LogicExpanderStateSnapshot_T
+{
+    uint16_t device_address_7bit;
+    uint8_t  olat_a;
+    uint8_t  olat_b;
+} LogicExpanderStateSnapshot_T;
+
 /**-----------------------------------------------------------------------------
  *  Public Function Prototypes
  *------------------------------------------------------------------------------
  */
+
+LogicExpanderStatus_T expander_self_config( void );
+
+LogicExpanderStatus_T expander_load_control_bit( uint8_t expander_index, LogicExpanderPort_T port,
+                                                 uint8_t bit_index, bool bit_value );
+
+LogicExpanderStatus_T expander_send_control_bits( void );
+
+LogicExpanderStatus_T expander_get_state_snapshot( uint8_t                       expander_index,
+                                                   LogicExpanderStateSnapshot_T* out_snapshot );
 
 #ifdef __cplusplus
 }
