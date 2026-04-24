b7325ce083383569a511036c0d539b2adeb52451
Angus Corr Desktop <angus.corr@monash.edu>
2026-04-24 10:41:58 +1000

    DEV-78 SPI - Fixing up issues with rebase

--- f446ze_cubeide_project/Core/Src/stm32f4xx_it.c
+++ f446ze_cubeide_project/Core/Src/stm32f4xx_it.c
@@ -64,7 +64,6 @@ extern CAN_HandleTypeDef hcan2;
 extern FMPI2C_HandleTypeDef hfmpi2c1;
 extern DMA_HandleTypeDef hdma_i2c2_rx;
 extern DMA_HandleTypeDef hdma_i2c2_tx;
-extern I2C_HandleTypeDef hi2c1;
 extern I2C_HandleTypeDef hi2c2;
 extern DMA_HandleTypeDef hdma_quadspi;
 extern DMA_HandleTypeDef hdma_spi1_tx;
@@ -302,20 +301,6 @@ void TIM4_IRQHandler(void)
   /* USER CODE BEGIN TIM4_IRQn 1 */
 
   /* USER CODE END TIM4_IRQn 1 */
-}
-
-/**
-  * @brief This function handles I2C1 event interrupt.
-  */
-void I2C1_EV_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_EV_IRQn 0 */
-
-  /* USER CODE END I2C1_EV_IRQn 0 */
-  HAL_I2C_EV_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_EV_IRQn 1 */
-
-  /* USER CODE END I2C1_EV_IRQn 1 */
 }
 
 /**
