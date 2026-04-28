ada82dd5bf5b9535c25623abd91cae4b6f102777
CallumR2 <craf0002@student.monash.edu>
2026-04-28 13:56:48 +1000

    DEV-84 Moved public structs to header, updated config function to use hw_timer.c, added mock types

--- src/hardware_low_level/hw_pwm_capture/hw_pwm_capture.h
+++ src/hardware_low_level/hw_pwm_capture/hw_pwm_capture.h
@@ -32,6 +32,32 @@ extern "C"
  *------------------------------------------------------------------------------
  */
 
+typedef enum
+{
+    HW_PWM_CAPTURE_LV_3V3 = 0U,  // Capture voltage level at 3.3V (Hardware state)
+    HW_PWM_CAPTURE_LV_5V,        // Capture voltage level at 5V (Hardware state)
+    HW_PWM_CAPTURE_HV_12V,       // Capture voltage level at 12V (Hardware state)
+    HW_PWM_CAPTURE_HV_24V        // Capture voltage level at 24V (Hardware state)
+} HwPWMCaptureMode_T;
+
+typedef enum
+{
+    HW_PWM_CAPTURE_CHANNEL_1 = 0U,  // First PWM capture channel
+    HW_PWM_CAPTURE_CHANNEL_2        // Second PWM capture channel
+} HwPWMCaptureChannel_T;
+
+typedef struct
+{
+    HwPWMCaptureMode_T mode;        // Desired capture mode (voltage level)
+    bool               is_enabled;  // Flag to indicate if capture is enabled for this channel
+} HwPWMCaptureConfig_T;
+
+typedef struct
+{
+    volatile uint32_t* period_ticks;
+    volatile uint32_t* high_ticks;
+} HwPWMCaptureResult_T;
+
 /**-----------------------------------------------------------------------------
  *  Public Typedefs / Enums / Structures
  *------------------------------------------------------------------------------
@@ -41,6 +67,11 @@ extern "C"
  *  Public Function Prototypes
  *------------------------------------------------------------------------------
  */
+
+bool HW_PWM_Capture_Configure_Channel( HwPWMCaptureChannel_T       channel,
+                                       const HwPWMCaptureConfig_T* config );
+
+void HW_PWM_Capture_Get_Result( HwPWMCaptureChannel_T channel, HwPWMCaptureResult_T* result );
 
 #ifdef __cplusplus
 }
