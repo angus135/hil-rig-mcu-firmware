bc5a3afe87c9518cb34aa9a591e4b9440539d49c
cpas0004 <cpas0004@student.monash.edu>
2026-05-02 22:31:22 +1000

    DEV-80 Attempt to manually handle nCS line for SPI

--- f446ze_cubeide_project/Core/Inc/main.h
+++ f446ze_cubeide_project/Core/Inc/main.h
@@ -87,6 +87,8 @@ void Error_Handler(void);
 #define SPI1_CS_TEST_GPIO_Port GPIOE
 #define LD3_Pin GPIO_PIN_14
 #define LD3_GPIO_Port GPIOB
+#define nCS_Pin GPIO_PIN_15
+#define nCS_GPIO_Port GPIOB
 #define STLK_RX_Pin GPIO_PIN_8
 #define STLK_RX_GPIO_Port GPIOD
 #define STLK_TX_Pin GPIO_PIN_9
