533fe6c774185c6387d22b7f8fcf0ff0a8ab4bce
Angus Corr Desktop <angus.corr@monash.edu>
2026-05-05 19:23:34 +1000

    DEV-78 SPI - Updating interrupt handlers in main.c

--- f446ze_cubeide_project/Core/Src/main.c
+++ f446ze_cubeide_project/Core/Src/main.c
@@ -204,16 +204,59 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 extern PCD_HandleTypeDef  hpcd_USB_OTG_FS;
-extern ADC_HandleTypeDef  hadc1;
-extern SPI_HandleTypeDef  hspi1;
-extern TIM_HandleTypeDef  htim2;
-extern DMA_HandleTypeDef  hdma_usart3_rx;
-extern DMA_HandleTypeDef  hdma_usart3_tx;
 extern UART_HandleTypeDef huart2;
-extern UART_HandleTypeDef huart3;
+extern TIM_HandleTypeDef htim6;
+extern TIM_HandleTypeDef htim7;
+extern TIM_HandleTypeDef htim11;
+extern TIM_HandleTypeDef htim1;
 /******************************************************************************/
 /*           Cortex-M4 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
+
+/**
+  * @brief This function handles TIM1 trigger and commutation interrupts and TIM11 global interrupt.
+  */
+void TIM1_TRG_COM_TIM11_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_TRG_COM_TIM11_IRQn 0 */
+
+  /* USER CODE END TIM1_TRG_COM_TIM11_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  HAL_TIM_IRQHandler(&htim11);
+  /* USER CODE BEGIN TIM1_TRG_COM_TIM11_IRQn 1 */
+
+  /* USER CODE END TIM1_TRG_COM_TIM11_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM6 global interrupt and DAC1, DAC2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim6);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM7 global interrupt.
+  */
+void TIM7_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM7_IRQn 0 */
+
+  /* USER CODE END TIM7_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim7);
+  /* USER CODE BEGIN TIM7_IRQn 1 */
+
+  /* USER CODE END TIM7_IRQn 1 */
+}
+
+
 /**
  * @brief This function handles Non maskable interrupt.
  */
@@ -322,27 +365,6 @@ void SysTick_Handler( void )
        /* USER CODE BEGIN SysTick_IRQn 1 */
 
     /* USER CODE END SysTick_IRQn 1 */
-}
-
-/******************************************************************************/
-/* STM32F4xx Peripheral Interrupt Handlers                                    */
-/* Add here the Interrupt Handlers for the used peripherals.                  */
-/* For the available peripheral interrupt handler names,                      */
-/* please refer to the startup file (startup_stm32f4xx.s).                    */
-/******************************************************************************/
-
-/**
- * @brief This function handles SPI1 global interrupt.
- */
-void SPI1_IRQHandler( void )
-{
-    /* USER CODE BEGIN SPI1_IRQn 0 */
-
-    /* USER CODE END SPI1_IRQn 0 */
-    HAL_SPI_IRQHandler( &hspi1 );
-    /* USER CODE BEGIN SPI1_IRQn 1 */
-
-    /* USER CODE END SPI1_IRQn 1 */
 }
 
 /**
