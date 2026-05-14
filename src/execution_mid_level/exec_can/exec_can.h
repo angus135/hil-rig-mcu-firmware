4862b7bfe7070e852655bd88b76310c2d5e5cb89
tvog0001 <166362198+tvog0001@users.noreply.github.com>
2026-05-15 08:19:06 +1000

    DEV-74-CAN
    Can mid level drivers

--- src/execution_mid_level/exec_can/exec_can.h
+++ src/execution_mid_level/exec_can/exec_can.h
@@ -26,6 +26,8 @@ extern "C"
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "hw_can.h"
+
 /**-----------------------------------------------------------------------------
  *  Public Defines / Macros
  *------------------------------------------------------------------------------
@@ -40,43 +42,60 @@ extern "C"
  *  Public Function Prototypes
  *------------------------------------------------------------------------------
  */
-/**
- * @brief recieves data and stores it in rxData (8 bytes) over the hcan CAN channel 1
- *
- * @param rxData pointer to array of 8 bytes sections of available storage
- * @param length the number of 8 byte sections in rxData (the amount of CAN packages being read)
- *
- * Uses HAL to receive message over CAN channel 1
- */
-int EXEC_CAN_recieve1( uint8_t** rxData, uint32_t length );
 
 /**
- * @brief recieves data and stores it in rxData (8 bytes) over the hcan CAN channel 2
+ * @brief Activates can channel 1 to immidiatley begin sending messages from the tx buffer
  *
- * @param rxData pointer to array of 8 bytes sections of available storage
- * @param length the number of 8 byte sections in rxData (the amount of CAN packages being read)
- *
- * Uses HAL to receive message over CAN channel 2
  */
-int EXEC_CAN_recieve2( uint8_t** rxData, uint32_t length );
+void EXEC_CAN_Tx_Trigger1();
 
 /**
- * @brief transmits a number of the txData (8 bytes) over CAN channel 1
+ * @brief Activates can channel 2 to immidiatley begin sending messages from the tx buffer
  *
- * @param txData pointer to array of 8 bytes sections of data
- *
- * Uses HAL to transmit message over CAN channel 1
  */
-int EXEC_CAN_transmit1( uint8_t** txData, uint32_t length );
+void EXEC_CAN_Tx_Trigger2();
 
 /**
- * @brief transmits a number of the txData (8 bytes) over CAN channel 2
+ * @brief Writes a number of 8 byte packets (source) to the tx buffer of channel 1
  *
- * @param txData pointer to array of 8 bytes sections of data
+ * @param source an array of arrays, type:
+uint8_t can_tx_buffer1[X][CAN_PACKET_SIZE];
+ * @param length the number of can packets to be written (seen as X above)
  *
- * Uses HAL to transmit message over CAN channel 2
+ * @return 0 if the write was succesful, 1 otherwise. (partially succesful = 1)
  */
-int EXEC_CAN_transmit2( uint8_t** txData, uint32_t length );
+uint16_t EXEC_CAN_Load_Tx1( uint8_t source[][CAN_PACKET_SIZE], uint16_t length );
+
+/**
+ * @brief Writes a number of 8 byte packets (source) to the tx buffer of channel 2
+ *
+ * @param source an array of arrays, type:
+uint8_t can_tx_buffer1[X][CAN_PACKET_SIZE];
+ * @param length the number of can packets to be written (seen as X above)
+ *
+ * @return 0 if the write was succesful, 1 otherwise. (partially succesful = 1)
+ */
+uint16_t EXEC_CAN_Load_Tx2( uint8_t source[][CAN_PACKET_SIZE], uint16_t length );
+
+/**
+ * @brief Reads values from the rx channel 1 buffer one at a time and places them in dest
+ *
+ * @param dest pointer to array of 8 bytes sections of available storage
+ * @param len the number of 8 byte sections in dest (the amount of CAN packages being read)
+ *
+ * @return the number of entries read from the rx buffer (can be 0)
+ */
+uint16_t EXEC_CAN_Read_Rx1_Buffer( uint8_t dest[][CAN_PACKET_SIZE], uint16_t len );
+
+/**
+ * @brief Reads values from the rx channel 2 buffer one at a time and places them in dest
+ *
+ * @param dest pointer to array of 8 bytes sections of available storage
+ * @param len the number of 8 byte sections in dest (the amount of CAN packages being read)
+ *
+ * @return the number of entries read from the rx buffer (can be 0)
+ */
+uint16_t EXEC_CAN_Read_Rx2_Buffer( uint8_t dest[][CAN_PACKET_SIZE], uint16_t len );
 
 #ifdef __cplusplus
 }
