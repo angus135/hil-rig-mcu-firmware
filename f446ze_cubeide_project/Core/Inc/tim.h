a64693ab075711f217fe24271631c4eb22c12e07
tvog0001 <166362198+tvog0001@users.noreply.github.com>
2026-05-27 09:39:31 +1000

    DEV-85-PWM-Output
    Update from main IOC file

--- f446ze_cubeide_project/Core/Inc/tim.h
+++ f446ze_cubeide_project/Core/Inc/tim.h
@@ -36,6 +36,18 @@ extern TIM_HandleTypeDef htim1;
 
 extern TIM_HandleTypeDef htim2;
 
+extern TIM_HandleTypeDef htim3;
+
+extern TIM_HandleTypeDef htim4;
+
+extern TIM_HandleTypeDef htim5;
+
+extern TIM_HandleTypeDef htim6;
+
+extern TIM_HandleTypeDef htim7;
+
+extern TIM_HandleTypeDef htim11;
+
 extern TIM_HandleTypeDef htim12;
 
 extern TIM_HandleTypeDef htim13;
@@ -48,6 +60,12 @@ extern TIM_HandleTypeDef htim14;
 
 void MX_TIM1_Init(void);
 void MX_TIM2_Init(void);
+void MX_TIM3_Init(void);
+void MX_TIM4_Init(void);
+void MX_TIM5_Init(void);
+void MX_TIM6_Init(void);
+void MX_TIM7_Init(void);
+void MX_TIM11_Init(void);
 void MX_TIM12_Init(void);
 void MX_TIM13_Init(void);
 void MX_TIM14_Init(void);
