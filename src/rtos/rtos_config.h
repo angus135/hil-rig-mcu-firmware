2dc217ae67135c8e9a7ddccb24ed8f53552e2484
CallumR2 <craf0002@student.monash.edu>
2026-08-06 14:26:12 +1000

    DEV-129 Clang Format

--- src/rtos/rtos_config.h
+++ src/rtos/rtos_config.h
@@ -197,8 +197,8 @@ SemaphoreHandle_t xSemaphoreCreateMutexStatic( StaticSemaphore_t* mutex_buffer )
 SemaphoreHandle_t xSemaphoreCreateBinaryStatic( StaticSemaphore_t* semaphore_buffer );
 BaseType_t        xSemaphoreTake( SemaphoreHandle_t semaphore, TickType_t ticks_to_wait );
 BaseType_t        xSemaphoreGive( SemaphoreHandle_t semaphore );
-BaseType_t xSemaphoreGiveFromISR( SemaphoreHandle_t semaphore,
-                                  BaseType_t* higher_priority_task_woken );
+BaseType_t        xSemaphoreGiveFromISR( SemaphoreHandle_t semaphore,
+                                         BaseType_t*       higher_priority_task_woken );
 
 // queue.c functions
 
