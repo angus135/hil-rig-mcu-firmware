70249753cffe8cecababf5a3a9d248bb1404797b
CallumR2 <craf0002@student.monash.edu>
2026-09-15 09:07:27 +1000

    DEV-162 instruction message handler files

---
+++ src/host_interface/instruction_message_handler.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+ *  File:       instruction_message_handler.h
+ *  Author:     Callum Rafferty
+ *  Created:    15-Sep-2026
+ *
+ *  Description:
+ *      Public interface for handling incoming application instruction messages
+ *      and converting them to canonical Flash Manager instruction records.
+ *
+ *  Notes:
+ *      None
+ ******************************************************************************/
+
+#ifndef INSTRUCTION_MESSAGE_HANDLER_H
+#define INSTRUCTION_MESSAGE_HANDLER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**-----------------------------------------------------------------------------
+ *  Includes
+ *------------------------------------------------------------------------------
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/**-----------------------------------------------------------------------------
+ *  Public Defines / Macros
+ *------------------------------------------------------------------------------
+ */
+
+/**-----------------------------------------------------------------------------
+ *  Public Typedefs / Enums / Structures
+ *------------------------------------------------------------------------------
+ */
+
+/**-----------------------------------------------------------------------------
+ *  Public Function Prototypes
+ *------------------------------------------------------------------------------
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INSTRUCTION_MESSAGE_HANDLER_H */
