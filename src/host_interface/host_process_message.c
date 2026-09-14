899da0eb084edaaff6663351fd9ed530326ba23d
tvog0001 <166362198+tvog0001@users.noreply.github.com>
2026-09-15 08:18:40 +1000

    DEV-162
    Implement response message response

--- src/host_interface/host_process_message.c
+++ src/host_interface/host_process_message.c
@@ -328,15 +328,28 @@ HOST_Interface_Status_T HOST_INTERFACE_process_Variable_Result_Data(
     return HOST_INTERFACE_STATUS_NOT_IMPLEMENTED;
 }
 
-HOST_Interface_Status_T HOST_INTERFACE_process_Response( const HIL_Application_Message_T* recent_received_message,
-                                      HIL_Application_Message_T*  response_message,
-                                      bool*                       response_required, uint8_t* data, size_t data_size );
+HOST_Interface_Status_T
+HOST_INTERFACE_process_Response( const HIL_Application_Message_T* recent_received_message,
+                                 HIL_Application_Message_T*       response_message,
+                                 bool* response_required, uint8_t* data, size_t data_size )
+{
+    ( void )recent_received_message;
+    ( void )data;
+    (void) data_size;
+    // Host device should never be sending a response
+    // report error to host device
+    HOST_INTERFACE_Default_Error( response_message );
+    *response_required = true;
+    return HOST_INTERFACE_STATUS_OK;
+}
 
 HOST_Interface_Status_T
 HOST_INTERFACE_process_Error( const HIL_Application_Message_T* recent_received_message,
                               HIL_Application_Message_T* response_message, bool* response_required,
                               uint8_t* data, size_t data_size )
 {
+    ( void ) data;
+    (void) data_size;
     RunStateFaultReason_T fault = RUN_STATE_FAULT_EXTERNAL_REQUEST;
     switch ( recent_received_message->body.error.category )
     {
