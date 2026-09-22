0c4e3f2acf1fad9f2593eaa0185dd767bfb2dbc3
tvog0001 <166362198+tvog0001@users.noreply.github.com>
2026-09-22 21:04:38 +1000

    DEV-162
    Fix state change request

--- src/host_interface/host_process_message.c
+++ src/host_interface/host_process_message.c
@@ -197,7 +197,7 @@ HOST_Interface_Status_T HOST_INTERFACE_request_state_tranistion( RunState_T expe
     {
         return HOST_INTERFACE_STATUS_UNSUPPORTED_MESSAGE;
     }
-
+    // TODO change to use a timeout number instead of number of tries
     for ( uint8_t i = 0; i < num_trys; i++ )
     {
         RunStateManagerStatus_T run_state_status = { 0 };
@@ -844,8 +844,9 @@ HOST_Interface_Status_T HOST_INTERFACE_process_Finalize_Test_Upload(
     ( void )data;
     ( void )data_size;
     // Request transition to CONFIGURATION
+    // TODO change 3000 back to 4
     HOST_Interface_Status_T status = HOST_INTERFACE_request_state_tranistion(
-        RUN_STATE_CONFIGURATION, HOST_REQUEST_CONFIGURATION, 3000, *expected_tick_count );
+        RUN_STATE_ARMED, HOST_REQUEST_CONFIGURATION, 3000, *expected_tick_count );
     if ( status == HOST_INTERFACE_STATUS_UNSUPPORTED_MESSAGE )
     {
         // Construct the error message
